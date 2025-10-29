f8fa447cda23e4a4979f98d3a70e410eb5315d81
Bigjoe <bigjoefilm0@gmail.com>
2025-10-29 07:36:29 +0100

    fix: correct database path

--- src/env.js
+++ src/env.js
@@ -7,9 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    // DATABASE_URL: z.string().optional().default("file:./dev.db"),
-    DATABASE_URL: z.string().optional(),
-
+    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -29,7 +27,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL || "file:./dev.db",
+    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
